37e8142e1f18a200f166b5ba878d578f62465d9f
Tim <89241508+timfor@users.noreply.github.com>
2023-08-23 21:25:58 +0300

    Add files via upload

--- inputs/config.ts
+++ inputs/config.ts
@@ -3,8 +3,6 @@ const config = {
   numThreads: 4,
   ref: "",
   proxyType: "http", // socks5 http
-  customDelay: 30000,
-  waitOtpDelay: 90000,
 };
 
 module.exports = config;
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,73 @@
-const { RecaptchaV2Task } = require("node-capmonster");
-const { CookieJar } = require("tough-cookie");
 const axios = require("axios");
 const generator = require("generate-password");
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
-const cheerio = require("cheerio");
-const puppeteer = require("puppeteer");
-
-const jar = new CookieJar();
-
 const delay = require("./delay.ts");
-
 const { random } = require("user-agents");
 const { SocksProxyAgent } = require("socks-proxy-agent");
 const { HttpsProxyAgent } = require("https-proxy-agent");
+const config = require("../inputs/config.ts");
+const fs = require("fs");
+const {
+  Worker,
+  workerData,
+  isMainThread,
+  parentPort,
+} = require("worker_threads");
+const { faker } = require('@faker-js/faker');
 
-const genPassword = () => {
-  return generator.generate({
-    length: 12,
-    numbers: true,
+const RecaptchaV2TaskKey = config.ANTICAPTCHA_API_KEY;
+
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+
+const csvWriter = createCsvWriter({
+  path: "./result.csv",
+  header: [
+    { id: "email", title: "Email" },
+    { id: "proxy", title: "Proxy" },
+    {id:"firstName",title:"firstName"},
+    {id:"lastName",title:"lastName"},
+  ],
+  append: true,
+});
+
+
+
+function parseEmails(filePath) {
+  const lines = fs.readFileSync(filePath, "utf8").split("\n");
+  const emails = [];
+
+  lines.forEach((line) => {
+    const [email, imapPass] = line.split(":");
+    emails.push({ email: email.trim(), imapPass: imapPass.trim() });
   });
-};
 
-const RecaptchaV2TaskKey = "";
-const proxy = "";
+  return emails;
+}
+function parseProxies(filePath) {
+  const lines = fs.readFileSync(filePath, "utf8").split("\n");
+  const proxies = [];
+
+  lines.forEach((line) => {
+    const proxy = line.trim();
+    proxies.push(proxy);
+  });
+
+  return proxies;
+}
+const emails = parseEmails("./inputs/emails.txt");
+const proxies = parseProxies("./inputs/proxies.txt");
 
 const SITEURL = "https://app.lorescan.com/";
 const SITE_KEY = "x19joXI_IeQnFJ7YnfDapSZq";
 
-async function main() {
-  const regUrl = "https://app.lorescan.com/signup";
+async function auth(email,proxy) {
+  const regUrl = "https://app.lorescan.com/api/auth/createUser";
 
   const headers = {
     authority: "app.lorescan.com",
     accept: "*/*",
     "accept-language":
       "en-US,en;q=0.9,uk;q=0.8,ru-RU;q=0.7,ru;q=0.6,en-GB;q=0.5",
-    referer: `https://app.lorescan.com/signup?ref=expLOREr-offmrplb`,
+    referer: `https://app.lorescan.com/signup?ref=${config.ref}`,
     "sec-ch-ua":
       '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
     "sec-ch-ua-mobile": "?0",
@@ -48,34 +80,62 @@ async function main() {
 
   const session = axios.create({
     headers: headers,
-    httpsAgent: new HttpsProxyAgent(`http://${proxy}`),
+    httpsAgent: config.proxyType==='http'? new HttpsProxyAgent(`http://${proxy}`):new SocksProxyAgent(`socks5://${proxy}`),
   });
+const firstName=faker.person.firstName()
+const lastName =faker.person.lastName()
+  const data = {
+    "first_name": firstName,
+    "last_name": lastName,
+    "email": email.email,
+    "password": "n/a",
+    "referral_code": config.ref
+  }
+  const res = await session.post(regUrl, data)
+  const resultData = [
+    {
+      email: email.email,
+      proxy: proxy,
+      firstName:firstName,
+      lastName:lastName
+    },
+  ];
+  await csvWriter
+    .writeRecords(resultData)
+    .then(() => {
+      console.log("CSV file has been saved.");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
-  // const getResponse = await session.get("https://app.lorescan.com/signup");
-  // const $ = cheerio.load(getResponse.data);
-  // const captchaSettings = $('input[name="captcha_settings"]').val();
-  // const client = new RecaptchaV2Task(RecaptchaV2TaskKey);
-  // const task = client.task({
-  //   websiteURL: SITEURL,
-  //   websiteKey: SITE_KEY,
-  // });
-  // const taskId = await client.createWithTask(task);
-  // const result = await client.joinTaskResult(taskId);
-
-  // console.log("ac result", result);
-
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-
-  await page.goto("https://app.lorescan.com/signup");
-
-  await page.type('input[id="email"]', "test@test.com");
-
-  const html = await page.content();
-  // await delay(10000);
-  // console.log(html);
-
-  // await browser.close();
+  console.log(res.data.status)
 }
 
-main();
+
+if (isMainThread) {
+  for (let i = 0; i < emails.length; i++) {
+    const worker = new Worker(__filename, {
+      workerData: { email: emails[i], proxy: proxies[i] },
+    });
+    worker.on("message", (message) => {
+      console.log(message);
+    });
+    worker.on("error", (error) => {
+      console.error(error);
+    });
+    worker.on("exit", (code) => {
+      if (code !== 0) {
+        console.error(`Thread Exit ${code}`);
+      }
+    });
+
+    // Остановка создания потоков, когда достигнуто указанное количество
+    if (i + 1 === config.numThreads) {
+      break;
+    }
+  }
+} else {
+  const { email, proxy } = workerData;
+  auth(email, proxy);
+}
